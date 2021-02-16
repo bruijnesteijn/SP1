fb6eeb1ede24625ed00de0701f9649bcaf864dac
Bruijnesteijn, Bart (ITCDEC) - KLM <bart.bruijnesteijn@klm.com>
2021-02-16 14:43:13 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -16,16 +16,15 @@ let package = Package(
             targets: ["SP1"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
-        .package(url: "https://github.com/realm/realm-cocoa.git", .upToNextMajor(from: "5.4.0")),
-        .package(url: "https://github.com/bruijnesteijn/KLUIDesignElements.git", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/realm/SwiftLint.git", .branch("master")),
+        .package(url: "https://github.com/bruijnesteijn/SP3.git", .upToNextMajor(from: "0.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SP1",
-            dependencies: []),
+            dependencies: ["SP3"]),
         .testTarget(
             name: "SP1Tests",
             dependencies: ["SP1"]),
